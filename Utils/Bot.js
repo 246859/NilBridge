6e65e57d519a9ac88667a3993824a0f826e748c7
Lition802 <2959435045@qq.com>
2021-11-08 16:21:56 +0800

    重新上传

---
+++ Utils/Bot.js
@@ -0,0 +1,58 @@
+const { createClient } = require("oicq");
+const fs = require("fs");
+
+try{
+  fs.statSync('./Data/bot.json')
+}catch{
+  fs.writeFileSyn('./Data/bot.json',JSON.stringify({botqq:114514,protocol:3,group:{main:114154,chat:114514}}));
+}
+
+var cfg = JSON.parse(fs.readFileSync('./Data/bot.json','utf8'));
+
+
+NIL.bot = {};
+
+const account = cfg.botqq;
+const conf = {//机器人内部配置
+    platform: cfg.protocol,//QQ登录协议。1:安卓手机 2:安卓平板 3:安卓手表 4:MacOS 5:iPad
+    kickoff: false,
+    ignore_self: true,
+    resend: true,
+    brief: true		
+};
+const client = createClient(account,conf);
+
+client.on("system.online", () => NIL.Logger.info('[OICQ]',"登录成功!"));
+
+client.on("message", e => {
+  //console.log(e);
+  //client.pickGroup(808776416).sendMsg('nmsl');
+  //e.reply("hello world", true); //true表示引用对方的消息
+})
+
+client.on("system.login.qrcode", function (e) {
+  //扫码后按回车登录
+  process.stdin.once("data", () => {
+    this.login();
+  })
+}).login();
+
+NIL.bot.sendGroupMessage = function(group,msg){
+    if(client.status != 11) {NIL.Logger.warn('[OICQ]','插件在未登录时调用了API'); return;}
+    client.pickGroup(group).sendMsg(msg);
+}
+
+NIL.bot.sendFriendMessage = function(friend,msg){
+    if(client.status != 11){ NIL.Logger.warn('[OICQ]','插件在未登录时调用了API'); return;}
+    client.pickFriend(friend).sendMsg(msg);
+}
+
+NIL.bot.sendMainMessage = function(msg){
+  if(client.status != 11){ NIL.Logger.warn('[OICQ]','插件在未登录时调用了API'); return;}
+    client.pickGroup(cfg.group.main).sendMsg(msg);
+}
+
+NIL.bot.sendChatMessage = function(msg){
+  if(client.status != 11){ NIL.Logger.warn('[OICQ]','插件在未登录时调用了API'); return;}
+  client.pickGroup(cfg.group.chat).sendMsg(msg);
+}
---
+++ app.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+global.NIL = {};
+
+NIL.SERVERS = {};
+NIL.FUNC = {};
+NIL.TOOL = {};
+NIL.CLASS = {};
+
+require("./Utils/Logger");
+
+NIL.Logger.info('[NIL]','正在启动...');
+
+require("./Utils/AES");
+require("./Utils/MD5");
+require("./Utils/websocket");
+require("./Utils/initServers");
+require("./Func/onWSReceive");
+
+for(i in NIL.SERVERS){
+    NIL.Logger.info('[NIL]','loading server: '+i+`(${NIL.SERVERS[i].url})`);
+}
+NIL.Logger.info("[NIL]",`成功加载${Object.keys(NIL.SERVERS).length}个服务器`);
+
+NIL.Logger.info("[OICQ]","准备登录QQ....");
+NIL.Logger.info('[OICQ]',"扫码后回车即可登录");
+
+
+//require('./Utils/Bot')
+
+
+
+process.stdin.on('data',(input)=>{
+	if(input.toString('hex')=="73746f700d0a"){//歪比歪比？
+		NIL.Logger.info('[NIL]','准备退出');
+		setTimeout(function(){process.exit(0)},1000)
+	}
+});
